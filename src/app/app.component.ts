0dec8c78f4c91377ed1230018a89ecc83b5a3887
Ovcharenko <vikt.ovcharenko@gmail.com>
2020-10-11 00:31:34 +0300

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ng-blogspace';
-}
+export class AppComponent {}
