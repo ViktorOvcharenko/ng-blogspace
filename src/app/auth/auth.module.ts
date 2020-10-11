2e78c15f28e18b1032171d1bdcfb2ccad558961d
Ovcharenko <vikt.ovcharenko@gmail.com>
2020-10-11 10:55:50 +0300

    created mockup for RegistrationComponent

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 
 import * as fromComponents from './components';
+import {ReactiveFormsModule} from '@angular/forms';
+import {TranslateModule} from '@ngx-translate/core';
 
 @NgModule({
   declarations: [fromComponents.RegistrationComponent],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule,
+    TranslateModule
   ]
 })
 export class AuthModule { }
--- src/app/auth/components/registration/registration.component.ts
+++ src/app/auth/components/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.initForm();
   }
 
+  initForm(): void {
+    this.form = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.form.value);
+  }
 }
