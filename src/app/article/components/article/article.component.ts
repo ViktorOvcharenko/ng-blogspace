ca7f2ea8d4bccacb9ca5781e703d12ae0157126a
Ovcharenko <vikt.ovcharenko@gmail.com>
2020-10-24 00:37:06 +0300

    get async data and created inputs for article

--- src/app/article/components/article/article.component.ts
+++ src/app/article/components/article/article.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import * as fromSharedModels from '../../../shared/models';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.scss']
 })
-export class ArticleComponent  { }
+export class ArticleComponent  {
+  @Input() isLoading: boolean;
+  @Input() article: fromSharedModels.Article;
+  @Input() errors: fromSharedModels.BackendErrors;
+}
--- src/app/article/containers/article/article.component.ts
+++ src/app/article/containers/article/article.component.ts
@@ -1,21 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
+import { select, Store } from '@ngrx/store';
 import { ActivatedRoute, Params } from '@angular/router';
-import { Subject } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { GetArticle } from '../../store/article.actions';
+import {
+  getArticle,
+  getArticleErrors,
+  getArticleIsLoading
+} from '../../store/article.selectors';
+
+import * as fromSharedModels from '../../../shared/models';
 
 @Component({
   selector: 'app-article-container',
   templateUrl: './article.component.html',
 })
 export class ArticleComponent implements OnInit {
+  isLoading$: Observable<boolean>;
+  article$: Observable<fromSharedModels.Article>;
+  errors$: Observable<fromSharedModels.BackendErrors>;
   destroy$: Subject<void> = new Subject<void>();
 
   constructor(
     private store: Store,
     private route: ActivatedRoute
-  ) { }
+  ) {
+    this.isLoading$ = this.store.pipe(select(getArticleIsLoading));
+    this.article$ = this.store.pipe(select(getArticle));
+    this.errors$ = this.store.pipe(select(getArticleErrors));
+  }
 
   ngOnInit(): void {
     this.fetchArticle();
