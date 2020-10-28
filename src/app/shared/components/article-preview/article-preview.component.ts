7544d0ae7b4dbf771c80a1fc83de102191707d0c
Ovcharenko <vikt.ovcharenko@gmail.com>
2020-10-28 23:01:45 +0200

    created getters for article preview

--- src/app/shared/components/article-preview/article-preview.component.ts
+++ src/app/shared/components/article-preview/article-preview.component.ts
@@ -12,6 +12,42 @@ export class ArticlePreviewComponent  {
   @Input() article: fromSharedModels.Article;
   @Output() onHandleLike: EventEmitter<fromSharedModels.AddToFavorites> = new EventEmitter<fromSharedModels.AddToFavorites>();
 
+  get username(): string {
+    return this.article.author.username;
+  }
+
+  get image(): string {
+    return this.article.author.image;
+  }
+
+  get createdAt(): string {
+    return this.article.createdAt;
+  }
+
+  get favorited(): boolean {
+    return this.article.favorited;
+  }
+
+  get favoritesCount(): number {
+    return this.article.favoritesCount;
+  }
+
+  get slug(): string {
+    return this.article.slug;
+  }
+
+  get title(): string {
+    return this.article.title;
+  }
+
+  get description(): string {
+    return this.article.description;
+  }
+
+  get tagList(): fromSharedModels.Tag[] {
+    return this.article.tagList;
+  }
+
   handleLike(event: fromSharedModels.AddToFavorites): void {
     this.onHandleLike.emit(event);
   }
