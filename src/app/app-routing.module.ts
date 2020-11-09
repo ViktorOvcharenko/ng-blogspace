73123ad81d034434251705440cf6f832df7cc854
Ovcharenko <vikt.ovcharenko@gmail.com>
2020-11-09 22:20:43 +0200

    removed guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import * as fromSharedGuards from './shared/guards';
-
 const routes: Routes = [
   {
     path: '',
@@ -11,17 +9,14 @@ const routes: Routes = [
   },
   {
     path: 'feed',
-    canActivate: [ fromSharedGuards.AuthGuard ],
     loadChildren: () => import('./feed/feed.module').then(m => m.FeedModule),
   },
   {
     path: 'article',
-    canActivate: [ fromSharedGuards.AuthGuard ],
     loadChildren: () => import('./article/article.module').then(m => m.ArticleModule),
   },
   {
     path: 'settings',
-    canActivate: [ fromSharedGuards.AuthGuard ],
     loadChildren: () => import('./settings/settings.module').then(m => m.SettingsModule),
   },
   {
@@ -30,7 +25,6 @@ const routes: Routes = [
   },
   {
     path: 'profile',
-    canActivate: [ fromSharedGuards.AuthGuard ],
     loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule),
   },
   {
